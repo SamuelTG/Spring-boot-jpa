6b8df89031537cbadde41bebc77561e67005172f
Samuel Tanor-Gyedu <samuel.tanor-gyedu@sap.com>
2015-03-18 12:57:08 +0200

    adding new files

---
+++ src/main/java/com/sap/spring/jpa/Application.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.sap.spring.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * @author I303450
+ *
+ */
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+
+}
